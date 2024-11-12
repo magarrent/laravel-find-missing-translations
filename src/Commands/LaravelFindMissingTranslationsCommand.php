9d365a7681fac1b942b6d1a77858cf8ea327fda8
Marc Garcia Torrent <magarrent@gmail.com>
2024-11-12 12:30:26 +0100

    Improve command
    Sometimes on blade components, was missing keys

--- src/Commands/LaravelFindMissingTranslationsCommand.php
+++ src/Commands/LaravelFindMissingTranslationsCommand.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Console\Commands;
+namespace Magarrent\LaravelFindMissingTranslations\Commands;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Finder\Finder;
 
-class FindTranslationsCommand extends Command
+class LaravelFindMissingTranslationsCommand extends Command
 {
     protected $signature = 'find:translations';
 
